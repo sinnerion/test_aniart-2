f7f70cee36449e1b1421f6088a3b5363f4d2351e
sinnerion <a.dergachow@gmail.com>
2017-04-13 18:43:07 +0300

    letter version 2.0 (with foundation-email and inky)

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,30 @@
-'use strict';
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    inky = require('inky'),
+    inlineCss = require('gulp-inline-css'),
+    inlinesource = require('gulp-inline-source');
 
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const plumber = require('gulp-plumber');
-const cache = require('gulp-cache');
-const rename = require('gulp-rename');
-const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
-const pngout = require('imagemin-pngout');
-const browserSync = require('browser-sync');
-const rsync = require('gulp-rsync');
 
-gulp.task('b-sync', function () {
-  browserSync({
-    server: {
-      baseDir: 'src',
-    },
-    notify: false,
-  });
+//STYLES
+gulp.task('styles', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('html-minify', function () {
-  return gulp.src('src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('../dist'));
+//CONVERTE INKY
+gulp.task('inky', ['styles'], function() {
+  return gulp.src('./templates/**/*.html')
+    .pipe(inlinesource())
+    .pipe(inky())
+    .pipe(inlineCss({
+        preserveMediaQueries: true,
+        removeLinkTags: false
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
-
-gulp.task('images:min', function () {
-  return gulp.src('src/img/**/*')
-    .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngout()],
-    })))
-    .pipe(gulp.dest('../dist/img'));
+//WATCH
+gulp.task('default',function() {
+    gulp.watch(['./scss/**/*.scss', './templates/**/*.html'],['inky']);
 });
-
-gulp.task('images:copy', function () {
-  return gulp.src('src/img/**/*')
-      .pipe(gulp.dest('../dist/img'));
-});
-
-gulp.task('fonts', function () {
-  return gulp.src('src/fonts/**/*').pipe(gulp.dest('../dist/fonts'));
-});
-
-gulp.task('watch', ['images:copy', 'fonts', 'b-sync'], function () {
-  gulp.watch('src/*.html', browserSync.reload);
-});
-
-gulp.task('build', ['clearcache', 'html-minify', 'images:min']);
-
-gulp.task('clearcache', function () {
-  return cache.clearAll();
-});
-
-gulp.task('default', ['watch']);
